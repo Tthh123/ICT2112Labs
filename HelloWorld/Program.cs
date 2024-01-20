09e8b556afc87e0f80142d9c0cddf7d9fcbb5ba2
Tthh123 <gtthh123@gmail.com>
2024-01-20 14:04:54 +0800

    lab1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var projectTeam = "Happy Team";
+            Console.WriteLine("Hello everyone!");
+            Console.WriteLine($"My project team is {projectTeam}");
+            Console.WriteLine($"These are my team members:");
+            var memberNames = new List<string> { "Desmond", "Felipe",
+"Emillia", "Alan" };
+            foreach (var member in memberNames)
+            {
+                Console.WriteLine(member);
+            }
+        }
+    }
+}
---
+++ ICT2106WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ICT2106WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    public string ProjectTeamName = "BOAT";
+    public List<string> memberNames = new List<string> {"Desmond", "Felipe",
+"Emillia", "Alan"};
+}
